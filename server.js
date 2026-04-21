51c3c4fc40af57aed83e61e27c5572c9181d7700
Tallapudi Nandini Satya <119557891+20255-CM-055@users.noreply.github.com>
2026-04-21 11:39:56 +0530

    up-10

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 const predictRoutes = require("./routes/predictRoutes");
+const cron = require("node-cron");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 
 const authRoutes = require("./routes/authRoutes");
 const journalRoutes = require("./routes/journalRoutes");
 const app = express();
+let tokens = [];
 const quizRoutes = require("./routes/quizroutes");
 const activityRoutes = require("./routes/activityRoutes");
 const affirmationRoutes = require("./routes/affirmation");
@@ -35,6 +41,68 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 5000;
+
+// app.post("/save-token", (req, res) => {
+//   const { token } = req.body;
+
+//   if (!token) {
+//     return res.status(400).send("Token missing");
+//   }
+
+//   if (!tokens.includes(token)) {
+//     tokens.push(token);
+//   }
+
+//   console.log("📱 Stored tokens:", tokens);
+
+//   res.send("Token saved");
+// });
+
+app.post("/save-token", (req, res) => {
+  console.log("🔥 /save-token HIT (RENDER)");
+  console.log("📦 Body:", req.body);
+
+  const { token } = req.body;
+
+  if (!token) {
+    console.log("❌ Token missing");
+    return res.status(400).send("Token missing");
+  }
+
+  if (!tokens.includes(token)) {
+    tokens.push(token);
+  }
+
+  console.log("📱 Stored tokens:", tokens);
+
+  res.send("Token saved");
+});
+
+const sendNotification = async (token) => {
+  try {
+    await fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        to: token,
+        title: "Soulease Reminder 🧠",
+        body: "How are you feeling today?",
+      }),
+    });
+  } catch (err) {
+    console.log("❌ Notification error:", err);
+  }
+};
+
+cron.schedule("*/1 * * * *", () => {
+  console.log("⏰ Sending daily notifications...");
+  console.log("📦 Tokens:", tokens);
+  tokens.forEach(token => {
+    sendNotification(token);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
